264c05fc4d7eef40164fc5cb7fe3cfa36ca79cf3
ymizushi <mizushi@gmail.com>
2020-05-05 23:45:17 +0900

    fix code

--- rplugin/python3/tide_ja.py
+++ rplugin/python3/tide_ja.py
@@ -1,21 +1,59 @@
-import neovim
-import scrape from scraper
+import pynvim
+import datetime
 
-@neovim.plugin
+class Scraper:
+    PLACE_KEYS=(
+        "KW", # 川崎
+    )
+
+    def __init__(self):
+        pass
+
+    def scrape(self, place, date):
+        return {
+            '2020/05/05': {
+                status: None,
+                levels: {
+                  '03:18': 172,
+                  '15:45': 174,
+                  '09:36': 46,
+                  '21:46': 48,
+                }
+            },
+            '2020/05/06': {
+                status: 'full',
+                levels: {
+                  '03:18': 172,
+                  '15:45': 174,
+                  '09:36': 46,
+                  '21:46': 48,
+                }
+            }
+        }
+
+def scrape(place, date):
+    scraper = Scraper()
+    return scraper.scrape(place, date)
+
+@pynvim.plugin
 class TestPlugin(object):
     def __init__(self, nvim):
         self.nvim = nvim
 
-    @neovim.function("TestFunction", sync=True)
-    def testfunction(self, args):
-        self.nvim.current.line = ('Command with args: {}, range: {}' .format(args, range))
-
-    @neovim.command("TideJa", range='', nargs='*')
+    @pynvim.command("TideJa", range='', nargs='*')
     def testcommand(self, args, range):
-        date = 
-        if range == 2
+        place = 'KW'
+        date = datetime.date.today() # TODO: 文字列表現にする
+        tide = scrape(place, date)
+        if range == 2:
             place = args[0]
             date = args[1]
-        elif 
-        else
-        self.nvim.current.line = ('Command with place: {}, date: {}'.format(args, range))
+        elif range == 1:
+            date = args[0]
+        else:
+            pass
+        self.nvim.current.line = ('Command with place: {}, date: {}, tide: {}'.format(args, range, tide))
+
+
+
+
