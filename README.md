01a6fae22b5a90d63cfc7e39935a52c9a259d066
ymizushi <mizushi@gmail.com>
2020-05-06 16:32:02 +0900

    fix code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-nvim -u ./vimrc
+# tide-ja.vim
+
+tide-ja.vim is the vim plugin to display tidal table anywhere in Japan.
+
+# DEVELOPMENT
+
+1. `nvim -u ./vimrc`
+2. `:UpdateRemotePlugins`
--- rplugin/python3/tide_ja.py
+++ rplugin/python3/tide_ja.py
@@ -1,6 +1,8 @@
 import pynvim
 import datetime
 
+import lxml.html
+
 class Scraper:
     PLACE_KEYS=(
         "KW", # 川崎
@@ -9,11 +11,22 @@ class Scraper:
     def __init__(self):
         pass
 
-    def scrape(self, place, date):
+    def request():
+        pass
+
+    def scrape(self, place, start_date, end_date):
+        root = lxml.html.parse('http://qiita.com/advent-calendar/2014').getroot()
+        root.cssselect('title')[0].text
+        start_match = re.match(r"(\d{4})/(\d{2})/(\d{2})", start_date)
+
+        stn = place
+        ys = m.groups[0]
+        ys = m.groups[1]
+
         return {
             '2020/05/05': {
-                status: None,
-                levels: {
+                'status': 'full',
+                'levels': {
                   '03:18': 172,
                   '15:45': 174,
                   '09:36': 46,
@@ -21,8 +34,8 @@ class Scraper:
                 }
             },
             '2020/05/06': {
-                status: 'full',
-                levels: {
+                'status': 'full',
+                'levels': {
                   '03:18': 172,
                   '15:45': 174,
                   '09:36': 46,
@@ -31,9 +44,9 @@ class Scraper:
             }
         }
 
-def scrape(place, date):
+def scrape(place, start_date, end_date):
     scraper = Scraper()
-    return scraper.scrape(place, date)
+    return scraper.scrape(place, start_date, end_date)
 
 @pynvim.plugin
 class TestPlugin(object):
@@ -45,15 +58,11 @@ class TestPlugin(object):
         place = 'KW'
         date = datetime.date.today() # TODO: 文字列表現にする
         tide = scrape(place, date)
-        if range == 2:
-            place = args[0]
-            date = args[1]
-        elif range == 1:
-            date = args[0]
+        if args == 3:
+            place = args[1]
+            date = args[2]
+        elif range == 2:
+            place = args[1]
         else:
             pass
-        self.nvim.current.line = ('Command with place: {}, date: {}, tide: {}'.format(args, range, tide))
-
-
-
-
+        self.nvim.current.line = ('Command with place: {}, date: {}, tide: {}'.format(place, date, tide))
