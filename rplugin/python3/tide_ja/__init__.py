74183eceb41073eba472a0dcd9a80d664521c948
ymizushi <mizushi@gmail.com>
2020-05-13 15:15:53 +0900

    add place keys

--- rplugin/python3/tide_ja/__init__.py
+++ rplugin/python3/tide_ja/__init__.py
@@ -1,19 +1,16 @@
 import pynvim
 from datetime import datetime
 from .tide_table import tide_dict
+from .constants import PLACE_KEYS
 
 @pynvim.plugin
 class TestPlugin(object):
-    PLACE_KEYS= dict(
-      KW='川崎',
-    )
-
     def __init__(self, nvim):
         self.nvim = nvim
 
     def format_tide(self, place, date, tide):
         output = ["[tide-ja.vim]"]
-        output += [self.PLACE_KEYS[place]]
+        output += [PLACE_KEYS[place]]
         output += ["20{}-{}-{}".format(date[0], date[1], date[2])]
         highlows = tide["highs"] + tide["lows"]
         for e in sorted(highlows, key=lambda e: int(e[0][0])):
@@ -34,8 +31,6 @@ class TestPlugin(object):
             date = tuple(args[2].split('-'))
         elif range == 2:
             place = args[1]
-        else:
-            pass
 
         tide = tide_dict(place, date)
         tide_line = self.format_tide(place, date, tide)
